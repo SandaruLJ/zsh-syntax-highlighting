e569ece6f3a1f28c8b689272968bb2c5fac083a0
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2021-03-18 20:45:36 +0530

    Modify main highlighter

--- highlighters/main/main-highlighter.zsh
+++ highlighters/main/main-highlighter.zsh
@@ -34,7 +34,7 @@
 : ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=yellow}
 : ${ZSH_HIGHLIGHT_STYLES[suffix-alias]:=fg=green,underline}
 : ${ZSH_HIGHLIGHT_STYLES[global-alias]:=fg=cyan}
-: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=green,underline}
+: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=red,bold,underline}
 : ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[autodirectory]:=fg=green,underline}
 : ${ZSH_HIGHLIGHT_STYLES[path]:=underline}
@@ -46,12 +46,12 @@
 : ${ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]:=fg=magenta}
 : ${ZSH_HIGHLIGHT_STYLES[process-substitution]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]:=fg=magenta}
-: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=fg=cyan,bold}
+: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=fg=cyan,bold}
 : ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]:=fg=magenta}
-: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow}
-: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow}
+: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow,bold}
+: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow,bold}
 : ${ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]:=fg=yellow}
 : ${ZSH_HIGHLIGHT_STYLES[rc-quote]:=fg=cyan}
 : ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=cyan}
@@ -62,7 +62,7 @@
 : ${ZSH_HIGHLIGHT_STYLES[comment]:=fg=black,bold}
 : ${ZSH_HIGHLIGHT_STYLES[named-fd]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[numeric-fd]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[arg0]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[arg0]:=fg=green,bold}
 
 # Whether the highlighter should be called or not.
 _zsh_highlight_highlighter_main_predicate()
